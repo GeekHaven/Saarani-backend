4a8e9809fbbfae99430263e44e2ce0e7b1caf7f1
Shreyas Gupta <technology.shreyas@gmail.com>
2020-06-16 18:32:37 +0530

    Add initial routes GET /events GET /events/id POST /events

---
+++ app.js
@@ -0,0 +1,84 @@
+var admin = require("firebase-admin");
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://calender-app-b8f90.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("/");
+
+app.get('/events', function(req, res) {
+    var eventRef = ref.child("events");
+    eventRef.once("value", function(snapshot) {
+        res.json(snapshot.val());
+      }, function (error) {
+        console.log("The read failed: " + error.code);
+        res.json({error: error.code});
+      });
+})
+
+app.get('/events/:id', function(req, res) {
+    var eventRef = ref.child("events");
+    eventRef.once("value", function(snapshot) {
+        res.json(snapshot.val()[req.params.id]);
+      }, function (error) {
+        console.log("The read failed: " + error.code);
+        res.json({error: error.code});
+      });
+})
+
+app.post('/events', function (req, res) {
+    var idToken = req.body.token;
+    admin.auth().verifyIdToken(idToken)
+        .then(function (decodedToken) {
+            let uid = decodedToken.uid;
+            admin.auth().getUser(uid)
+                .then(function (userRecord) {
+                    var name = req.body.name;
+                    var byID = userRecord.uid;
+                    var byName = userRecord.displayName;
+                    var desc = req.body.desc;
+                    var venue = req.body.venue;
+                    var date = req.body.date;
+                    var time = req.body.time;
+                    var interestedUsers = {};
+                    var markedGoingUsers = {};
+                    var attachments = req.body.attachments;
+                    var eventRef = ref.child("events");
+                    var obj = {};
+                    obj.name = name;
+                    obj.byID = byID;
+                    obj.byName = byName;
+                    obj.desc = desc;
+                    obj.venue = venue;
+                    obj.date = date;
+                    obj.time = time;
+                    obj.interestedUsers = interestedUsers;
+                    obj.markedGoingUsers = markedGoingUsers;
+                    if(attachments) obj.attachments = attachments;
+                    eventRef.push(obj);
+                    res.json(obj);
+                })
+                .catch(function (error) {
+                    console.log('Error fetching user data:', error);
+                    res.json({error: error.code});
+                });
+        }).catch(function (error) {
+            console.log(error);
+            res.json({error: error.code});
+        });
+
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
