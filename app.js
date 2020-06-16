892240a94d181f14351ee2cba698bfb2a316e2e8
Shreyas Gupta <technology.shreyas@gmail.com>
2020-06-16 22:16:54 +0530

    Little refactoring, beautifying code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var admin = require("firebase-admin");
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-var bodyParser = require('body-parser')
+let admin = require("firebase-admin");
+let express = require('express')
+let app = express()
+let port = process.env.PORT || 3000
+let bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -18,50 +18,55 @@ admin.initializeApp({
 var db = admin.database();
 var ref = db.ref("/");
 
-app.get('/events', function(req, res) {
-    var eventRef = ref.child("events");
-    eventRef.once("value", function(snapshot) {
+app.get('/events', (req, res) => {
+    let eventRef = ref.child("events").once("value", (snapshot) => {
         res.json(snapshot.val());
-      }, function (error) {
+    }, (error) => {
         console.log("The read failed: " + error.code);
-        res.json({error: error.code});
-      });
+        res.json({
+            error: error.code
+        });
+    });
 })
 
-app.get('/events/:id', function(req, res) {
-    var eventRef = db.ref(`events/${req.params.id}`).once("value", snapshot => {
-        if (snapshot.exists()){
-            var event = snapshot.val();
+app.get('/events/:id', (req, res) => {
+    let eventRef = db.ref(`events/${req.params.id}`).once("value", snapshot => {
+        if (snapshot.exists()) {
+            let event = snapshot.val();
             res.json(event)
-        }
-        else{
-            console.log("Event does not exists")
+        } else {
+            console.log("Event does not exist");
+            res.json({
+                error: "Event does not exist"
+            });
         }
     }, (error) => {
         console.log("The read failed: " + error.code);
-        res.json({error: error.code});
+        res.json({
+            error: error.code
+        });
     })
 })
 
-app.post('/events', function (req, res) {
-    var idToken = req.body.token;
+app.post('/events', (req, res) => {
+    let idToken = req.body.token;
     admin.auth().verifyIdToken(idToken)
         .then(function (decodedToken) {
             let uid = decodedToken.uid;
             admin.auth().getUser(uid)
                 .then(function (userRecord) {
-                    var name = req.body.name;
-                    var byID = userRecord.uid;
-                    var byName = userRecord.displayName;
-                    var desc = req.body.desc;
-                    var venue = req.body.venue;
-                    var date = req.body.date;
-                    var time = req.body.time;
-                    var interestedUsers = {};
-                    var markedGoingUsers = {};
-                    var attachments = req.body.attachments;
-                    var eventRef = ref.child("events");
-                    var obj = {};
+                    let name = req.body.name;
+                    let byID = userRecord.uid;
+                    let byName = userRecord.displayName;
+                    let desc = req.body.desc;
+                    let venue = req.body.venue;
+                    let date = req.body.date;
+                    let time = req.body.time;
+                    let interestedUsers = {};
+                    let markedGoingUsers = {};
+                    let attachments = req.body.attachments;
+                    let eventRef = ref.child("events");
+                    let obj = {};
                     obj.name = name;
                     obj.byID = byID;
                     obj.byName = byName;
@@ -71,55 +76,70 @@ app.post('/events', function (req, res) {
                     obj.time = time;
                     obj.interestedUsers = interestedUsers;
                     obj.markedGoingUsers = markedGoingUsers;
-                    if(attachments) obj.attachments = attachments;
+                    if (attachments) obj.attachments = attachments;
                     eventRef.push(obj);
                     res.json(obj);
                 })
                 .catch(function (error) {
-                    console.log('Error fetching user data:', error);
-                    res.json({error: error.code});
+                    console.log("Error fetching user data:", error);
+                    res.json({
+                        error: error.code
+                    });
                 });
         }).catch(function (error) {
             console.log(error);
-            res.json({error: error.code});
+            res.json({
+                error: error.code
+            });
         });
 
 })
 
 
 app.delete('/events/:id', (req, res) => {
-    var idToken = req.body.token;
+    let idToken = req.body.token;
     admin.auth().verifyIdToken(idToken)
-        .then( (decodedToken) => {
+        .then((decodedToken) => {
             let uid = decodedToken.uid;
             admin.auth().getUser(uid)
-                .then( (userRecord) => {
-                    var eventRef = db.ref(`events/${req.params.id}`).once("value", snapshot => {
-                        if (snapshot.exists()){
+                .then((userRecord) => {
+                    let eventRef = db.ref(`events/${req.params.id}`).once("value", snapshot => {
+                        if (snapshot.exists()) {
                             var event = snapshot.val()
-                            if ( event.byID == userRecord.uid ) {
-                                db.ref(`events/${req.params.id}`).remove()
+                            if (event.byID == userRecord.uid) {
+                                db.ref(`events/${req.params.id}`).remove();
+                                res.sendStatus(200);
+                            } else {
+                                console.log("Not Authorized");
+                                res.json({
+                                    error: "Not Authorized"
+                                });
                             }
-                            else{
-                                console.log("Not Authorized")
-                            }
-                        }
-                        else{
-                            console.log("Event does not exists")
+                        } else {
+                            console.log("Event does not exist");
+                            res.json({
+                                error: "Event does not exist"
+                            });
                         }
                     }, (error) => {
                         console.log("The read failed: " + error.code);
-                        res.json({error: error.code});
+                        res.json({
+                            error: error.code
+                        });
                     })
                 })
                 .catch(function (error) {
                     console.log(error);
-                    res.json({error: error.code});
+                    res.json({
+                        error: error.code
+                    });
                 });
         })
         .catch(function (error) {
             console.log(error);
-            res.json({error: error.code});
+            res.json({
+                error: error.code
+            });
         });
 })
 
