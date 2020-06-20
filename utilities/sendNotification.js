36cc14d0c0f7e13cca851cd640def209dfff1ea8
Shreyas Gupta <technology.shreyas@gmail.com>
2020-06-20 15:21:12 +0530

    Custom notifs for event cancellation now works

--- utilities/sendNotification.js
+++ utilities/sendNotification.js
@@ -1,33 +1,17 @@
 let admin = require("../initFirebase.js");
 
-function sendNotification(title, body, image, subtext, topic, del=false) {
+function sendNotification(title, body, image, subtext, topic, del = false) {
     let message = {}
-    if(!del) {
-        message = {
-            data: {
-                title: title,
-                body: body,
-                image: image,
-                subtext: subtext
-            },
-            topic: topic
-        };
-    } else {
-        message = {
-            data: {
-                title: title,
-                body: body,
-                image: image,
-                subtext: subtext
-            },
-            notification: {
-                title: title,
-                body: body,
-                image: image
-            },
-            topic: topic
-        };
-    }
+    message = {
+        data: {
+            title: title,
+            body: body,
+            image: image,
+            subtext: subtext,
+            del: (del===true)? '1' : '0'
+        },
+        topic: topic
+    };
     admin.messaging().send(message)
         .then((response) => {
             console.log("Successfully sent message:", response);
