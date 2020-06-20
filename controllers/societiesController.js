775a602cbfd7d76ad1a9b533f879c628818240f0
untruly_noxius <chetangarg365@gmail.com>
2020-06-20 11:01:16 +0530

    seaperated code for authentication to authMiddleware

--- controllers/societiesController.js
+++ controllers/societiesController.js
@@ -1,4 +1,5 @@
 let admin = require("../initFirebase.js");
+let authMiddleware = require("../middlewares/authMiddleware")
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
@@ -61,65 +62,46 @@ router.get('/:uid/events', (req, res) => {
     })
 })
 
-router.post('/check', (req, res) => {
-    let idToken = req.body.token;
-    admin.auth().verifyIdToken(idToken)
-        .then(decodedToken => {
-            let uid = decodedToken.uid;
-            admin.auth().getUser(uid)
-                .then(userRecord => {
-                    const initials = userRecord.email.split("@")[0].split(".")[0];
-                    let societyRef = db.ref(`societies/${initials}`).once("value", snapshot => {
-                        if (snapshot.exists()) {
-                            let society = snapshot.val()
-                            if (!society.uid) {
-                                const updates = {};
-                                updates[`/societies/${initials}/uid`] = userRecord.uid;
-                                db.ref().update(updates);
-                            }
-                            res.json({
-                                society: true,
-                                uid: userRecord.uid
-                            })
-                        } else {
-                            const userRef = db.ref('users/').orderByChild("uid").equalTo(userRecord.uid).once("value", (snapshot) => {
-                                if (snapshot.exists()) {
-                                    console.log("User already exists")
-                                    console.log(snapshot.val())
-                                    res.json({
-                                        society: false,
-                                        uid: userRecord.uid
-                                    })
-                                } else {
-                                    const user = userRecord.toJSON();
-                                    const obj = {
-                                        uid: user.uid,
-                                        email: user.email,
-                                        displayName: user.displayName
-                                    };
-                                    admin.database().ref('users/').push(obj);
-                                    res.json({
-                                        society: false,
-                                        uid: user.uid
-                                    })
-                                }
-                            });
-                        }
-                    })
-                })
-                .catch(error => {
-                    console.log(error);
-                    res.json({
-                        error: error.code
-                    });
-                })
-        })
-        .catch(error => {
-            console.log(error);
+router.post('/check', authMiddleware, (req, res) => {
+    let userRecord = res.locals.userRecord;
+    const initials = userRecord.email.split("@")[0].split(".")[0];
+    let societyRef = db.ref(`societies/${initials}`).once("value", snapshot => {
+        if (snapshot.exists()) {
+            let society = snapshot.val()
+            if (!society.uid) {
+                const updates = {};
+                updates[`/societies/${initials}/uid`] = userRecord.uid;
+                db.ref().update(updates);
+            }
             res.json({
-                error: error.code
+                society: true,
+                uid: userRecord.uid
+            })
+        } else {
+            const userRef = db.ref('users/').orderByChild("uid").equalTo(userRecord.uid).once("value", (snapshot) => {
+                if (snapshot.exists()) {
+                    console.log("User already exists")
+                    console.log(snapshot.val())
+                    res.json({
+                        society: false,
+                        uid: userRecord.uid
+                    })
+                } else {
+                    const user = userRecord.toJSON();
+                    const obj = {
+                        uid: user.uid,
+                        email: user.email,
+                        displayName: user.displayName
+                    };
+                    admin.database().ref('users/').push(obj);
+                    res.json({
+                        society: false,
+                        uid: user.uid
+                    })
+                }
             });
-        })
+        }
+    })
 })
 
 module.exports = router;
